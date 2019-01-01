5a4e472705f156714003b466881ffa3bfca16edd
UsmanMalik04 <36415562+UsmanMalik04@users.noreply.github.com>
2019-01-01 15:30:51 +0500

    ddd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-const routes: Routes = [
+import { LoginComponent } from './Auth/login/login.component';
+import { SignupComponent } from './Auth/signup/signup.component';
+import { DashboardComponent } from './Dashboard/dashboard.component';
+import { ContractComponent } from './Dashboard/contract/contract.component';
+import {UploadPartsRequestComponent} from './Dashboard/upload-parts-request/upload-parts-request.component';
+import { SourcingComponent } from './Dashboard/sourcing/sourcing.component';
+import { ItemlistComponent } from './Dashboard/itemlist/itemlist.component';
 
+const routes: Routes = [
+  {path: '', component: LoginComponent}, 
+  {path: 'signup', component: SignupComponent}, 
+  {path: 'dashboard', component: DashboardComponent, children: [
+    //{path: "", redirectTo: "dashboard"}, 
+    { path: "reqs", component: ContractComponent },
+    { path: "uploadparts", component: UploadPartsRequestComponent }, 
+    { path: "sourcing", component: SourcingComponent }, 
+    { path: "itemlist", component: ItemlistComponent}
+  ]}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Auth/login/login.component';
+import { SignupComponent } from './Auth/signup/signup.component';
+import { DashboardComponent } from './Dashboard/dashboard.component';
+import { ContractComponent } from './Dashboard/contract/contract.component';
+import { MenuComponent } from './Dashboard/menu/menu.component';
+import { DlDateTimePickerDateModule } from 'angular-bootstrap-datetimepicker';
+import {UploadPartsRequestComponent } from './Dashboard/upload-parts-request/upload-parts-request.component';
+import { SourcingComponent } from './Dashboard/sourcing/sourcing.component';
+import { ItemlistComponent } from './Dashboard/itemlist/itemlist.component'
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    SignupComponent,
+    DashboardComponent,
+    ContractComponent,
+    MenuComponent,
+    UploadPartsRequestComponent,
+    SourcingComponent,
+    ItemlistComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    DlDateTimePickerDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
